9fb6832c56e7e6c5fe502f14ee3fe3edaf99c51d
edisonlu <edisonlu@tencent.com>
2022-07-16 10:32:26 +0800

    上传最小验证单元

---
+++ test/hello_tts.py
@@ -0,0 +1,16 @@
+from vnpy.event import EventEngine
+from vnpy_tts import TtsGateway
+
+username = "2865"
+password = "123456"
+
+
+if __name__ == "__main__":
+
+    event_engine = EventEngine()
+    gateway = TtsGateway()
+    gateway.connect({'用户名': username, '密码': password,
+                     '经纪商代码': '', '交易服务器': 'tcp://122.51.136.165:20002',
+                     '行情服务器': 'tcp://122.51.136.165:20004', '产品名称': '', '授权编码': ''})
+    input("Pause")
+    gateway.close()

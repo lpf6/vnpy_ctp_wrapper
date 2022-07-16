c4c6ff3922cee32fc20f1ee215a008eeba465dc4
edisonlu <edisonlu@tencent.com>
2022-07-16 15:47:49 +0800

    修改tts名称

--- test/hello_tts.py
+++ test/hello_tts.py
@@ -8,7 +8,7 @@ password = "123456"
 if __name__ == "__main__":
 
     event_engine = EventEngine()
-    gateway = TtsGateway(event_engine, "tts")
+    gateway = TtsGateway(event_engine, "TTS")
     gateway.connect({'用户名': username, '密码': password,
                      '经纪商代码': '', '交易服务器': 'tcp://122.51.136.165:20002',
                      '行情服务器': 'tcp://122.51.136.165:20004', '产品名称': '', '授权编码': ''})
